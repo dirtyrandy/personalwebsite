f2ce000f6e5820ac07ba01bd0f0a368a23bad8d0
Caleb <calebcook353@gmail.com>
2021-02-06 19:48:25 -0600

    added badging, made site work

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from config import template_directory, asset_directory, image_directory
+import os
 
 from flask import Flask
 from jinja2 import Template
 
+from config import template_directory
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ def root():
     template_file = next(
         file for file in template_directory.iterdir() if 'index.html' == file.name.lower()
     )
-    return Template(template_file.read_text()).render()
+    return Template(template_file.read_text()).render(cloudfront_url=os.getenv('cloudfront_url'))
 
 
 if __name__ == '__main__':
